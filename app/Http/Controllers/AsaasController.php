c191a032c566c45dea9a195b350b8f6675fb91c9
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2023-09-10 17:48:21 -0300

    projeto-base-ebook
    # APIs Ebook

--- app/Http/Controllers/AsaasController.php
+++ app/Http/Controllers/AsaasController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ebook;
 use App\Models\User;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Carbon\Carbon;
 
 use App\Models\Vendas;
 
@@ -30,8 +33,18 @@ class AsaasController extends Controller
                 } else {
                     return response()->json(['status' => 'error', 'response' => 'Venda Confirmada, mas sem tratamento!']);
                 }
-            } else {
-                return response()->json(['status' => 'error', 'response' => 'Venda não existe!']);
+            }
+
+            $ebook = Ebook::where('id_pay', $id)->first();
+            if ($ebook) {
+                $ebook->status = 'PAYMENT_CONFIRMED';
+                $ebook->save();
+
+                if($this->trataProduto($ebook->id_produto, $ebook->id_vendedor)){
+                    return response()->json(['status' => 'success', 'response' => 'Venda Confirmada!']);
+                } else {
+                    return response()->json(['status' => 'error', 'response' => 'Venda Confirmada, mas sem tratamento!']);
+                }
             }
         }
 
@@ -53,11 +66,83 @@ class AsaasController extends Controller
 
                 return true;
                 break;
+            case 2:
+                return true;
+                break;
+            case 3:
+                return true;
+                break;
             default:
                 return true;
         }
     }
 
+    public function geraPagamento(Request $request) {
+       $jsonData = $request->json()->all();
+
+       $email     = $jsonData['email'];
+       $cpf   = $jsonData['cpf'];
+       $pagamento = $jsonData['pagamento'];
+       $produto   = $jsonData['produto'];
+       $vendedor   = $jsonData['vendedor'];
+
+       $client = new Client();
+
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'access_token' => env('API_TOKEN'),
+            ],
+            'json' => [
+                'name'      => 'Cliente Diego Brazil',
+                'cpfCnpj'   => $cpf,
+            ],
+        ];
+
+        $response = $client->post(env('API_URL_ASSAS') . 'api/v3/customers', $options);
+        $body = (string) $response->getBody();
+        $data = json_decode($body, true);
+
+        if ($response->getStatusCode() === 200) {
+            $options = [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'access_token' => env('API_TOKEN'),
+                ],
+                'json' => [
+                    'customer' => $data['id'],
+                    'billingType' => $pagamento,
+                    'value' => $produto == 1 ? 9.99 : ($produto == 2 ? 19.99 : 9.99),
+                    'dueDate' => Carbon::now()->addDay()->format('Y-m-d'),
+                    'description' => 'BRA - Produtos',
+                ],
+            ];
+
+            $response = $client->post(env('API_URL_ASSAS') . 'api/v3/payments', $options);
+            $body = (string) $response->getBody();
+            $data = json_decode($body, true);
+
+            if ($response->getStatusCode() === 200) {
+
+                $ebook = Ebook::create([
+                    'cpf' => $cpf,
+                    'email' => $email,
+                    'valor' => $produto == 1 ? 9.99 : ($produto == 2 ? 19.99 : 9.99),
+                    'produto' => $produto,
+                    'status' => 'PENDING_PAY',
+                    'id_pay' => $data['id'],
+                    'id_vendedor' => $vendedor,
+                ]);
+                return [
+                    'paymentLink' => $data['invoiceUrl'],
+                ];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
 
 
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,13 @@ use App\Http\Controllers\AsaasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 //Webhook Assas
 Route::post('assas', [AsaasController::class, 'receberPagamento'])->name('assas');
+
+//Gera Pagamento
+Route::post('geraPagamento', [AsaasController::class, 'geraPagamento'])->name('geraPagamento');
+
